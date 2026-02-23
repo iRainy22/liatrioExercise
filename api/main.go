9f534616f2032e604af70b9971089ec0f1afdd2a
iRainy22 <rainier.ring@gmail.com>
2026-02-22 23:21:24 -0800

    API now returns message using a struct

--- api/main.go
+++ api/main.go
@@ -6,13 +6,19 @@ import (
     "github.com/gofiber/fiber/v3"
 )
 
+type Response struct {
+  Message   string `json:"message"`
+  Timestamp int64  `json:"timestamp"`
+}
+
+
 func main() {
     app := fiber.New()
 
     app.Get("/", func (c fiber.Ctx) error {
-        return c.JSON(fiber.Map{
-	    "message" : "My name is Rainier Ring",
-	    "timestamp" : time.Now().Unix(),
+        return c.JSON(Response{
+			Message:   "My name is Rainier Ring",
+			Timestamp: time.Now().Unix(),
 		})
     })
 
