1df033412c457b3da62d514f95e3945612241aeb
iRainy22 <rainier.ring@gmail.com>
2026-02-22 22:54:37 -0800

    API now returns a corrected message

--- api/main.go
+++ api/main.go
@@ -10,7 +10,10 @@ func main() {
     app := fiber.New()
 
     app.Get("/", func (c fiber.Ctx) error {
-        return c.SendString("Hello, World!")
+        return c.JSON(fiber.Map{
+		"message" : "My name is Rainier Ring",
+		"timestamp" : "_____",
+	})
     })
 
     log.Fatal(app.Listen(":3000"))
