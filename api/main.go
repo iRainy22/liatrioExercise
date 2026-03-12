189b8e9d3d60bd076f250e4b338a240d0877e805
iRainy22 <rainier.ring@gmail.com>
2026-03-11 21:59:49 -0700

    Demo test 2

--- api/main.go
+++ api/main.go
@@ -23,7 +23,7 @@ func main() {
 	//tells the app to display the JSON message at the address /
 	app.Get("/", func (c fiber.Ctx) error {
         return c.JSON(Response{
-			Message:   "My name is Rainier Ring",
+			Message:   "My name is George Jefferson James",
 			Timestamp: time.Now().UnixMilli(),
 		})
     })
